63db981e26cf53ba3570af5b99b2d33073fcb50b
AirVan21 <airvan21@gmail.com>
2014-11-23 02:20:14 +0300

    Corrections  after precompiling

--- USB/Modes/topm.asm
+++ USB/Modes/topm.asm
@@ -1,16 +1,17 @@
 	model tiny
 	.CODE
 	.386
-	org 100h   ;  Going to create .COM file
+	org 100h          ;  Going to create .COM file
 
 _:
+
 ; CONSTS
-CMOS_PORT   = 70h     ; port for CMOS memory access	
-STATUS_PORT = 64h     ; port for SHUTDOWN
+CMOS_PORT_ID   = 70h  ; Port for CMOS memory access	
+STATUS_PORT = 64h     ; Port for SHUTDOWN
 SHUT_DOWN   = 0FEh    ; CP SHUTDOWN commmand
-real_sp   dw
-real_ss   dw
-real_es   dw
+;real_sp   dw
+;real_ss   dw
+;real_es   dw
 
 	jmp start  ;  Marker to place, where program starts	
 	
@@ -18,7 +19,7 @@ start:
 
 set_prot_mode:
 	
-	call disable_interrapts  ; Disable Maskable && Non-Maskable interrupts
+	call disable_interrupts  ; Disable Maskable && Non-Maskable interrupts
 	;mov  [real_ss],ss       ; Save Stack Pointer
     ;mov  [real_es],es       ; 
 	;lgdt [GDTR]             ; Load GDT
@@ -26,47 +27,28 @@ set_prot_mode:
 	jmp outOfProg            ;
 	
 	
-set_real_mode:
-	mov [real_sp], sp       ; Saving SP
-
-	; CPU Shut Down
-    mov al,SHUT_DOWN
-    out STATUS_PORT,al
-	; Ожидаем сброса процессора
-
-wait_reset:
-    hlt
-    jmp     wait_reset
-		
-	; -->> В это место мы попадём после сброса процессора,
-	; теперь мы снова в реальном режим
-		
-	; Восстанавливаем указатель стека
-    mov     ss,[real_ss]
-    mov     sp,[real_sp]
-	; Восстанавливаем содержимое регистра es
-    mov     es,[real_es]
-	; Разрешаем прерывания
-	call enable_interrupts;
-	; Exit()
-	jmp outOfProg         ;
-	
 disable_interrupts:
-	xor eax, eax      ;
-	cli               ; Blocking Maskable Interrupts
-	in  al, CMOS_PORT ; Getting CMOS-byte
-	or  al, 80h       ; First bit for Non-Maskable
-	                  ; 0 - Enabled, 1 - Disables
-	out al, CMOS_PORT ; Disabling NMI 
-	ret               ;
+	xor eax, eax         ;
+	push dx, dx          ; Save dx
+	cli                  ; Blocking Maskable Interrupts
+	mov dl, CMOS_PORT_ID ; (Params should be registers)
+	in  al, dl           ; Getting CMOS-byte
+	or  al, 80h          ; First bit for Non-Maskable
+	                     ; 0 - Enabled, 1 - Disables
+	out al, dl           ; Disabling NMI 
+	pop dx               ; Recover dx
+	ret                  ;
 
 enable_interrupts:
-	xor eax, eax      ;
-	in  al, CMOS_PORT ; Getting CMOS-byte
-	and al, 7Fh       ; Setting first bit to zero 
-	                  ; 0 - Enables NMI
-	out al, CMOS_PORT ;
-	ret               ;
+	xor eax, eax         ;
+	push dx, dx          ; Save dx
+	mov dl, CMOS_PORT_ID ; 
+	in  al, dl           ; Getting CMOS-byte
+	and al, 7Fh          ; Setting first bit to zero 
+	                     ; 0 - Enables NMI
+	out al, dl           ;
+	pop dx               ; Recover dx
+	ret                  ;
 
 GDT_DESCR:
 
@@ -77,14 +59,15 @@ GDT:
 NULL_descr		db 0,0,0,0,0,0,0,0											; 0 zero descriptor
 CODE_descr		db 0FFh,0FFh,00h,00H,00H,10011010b, 11001111b,00h			; 1 code main code descriptor
 DATA_descr		db 0FFh, 0FFh, 00h, 00h, 00h, 10010010b, 11001111b	, 00h	; 2 data main descriptor
-GDT_16bitCS     db 0FFh, 0FFh, 0, 0, 0, 10011010b, 0, 0						; 3 16-bit code descriptor
-GDT_16bitDS     db 0FFh, 0FFh, 0, 0, 0, 10010010b, 0, 0						; 4 16-bit data descriptor
-gdt_free_cells  db (GDT_COUNT-1-5) DUP (6 DUP(0))                           ; Setting zeros for
+GDT_16bitCS		db 0FFh, 0FFh, 0, 0, 0, 10011010b, 0, 0						; 3 16-bit code descriptor
+GDT_16bitDS		db 0FFh, 0FFh, 0, 0, 0, 10010010b, 0, 0						; 4 16-bit data descriptor
+
+;gdt_free_cells         db (GDT_COUNT-1-5) DUP (6 DUP(0))                  ; Setting zeros for
 ;times(GDT_COUNT-1-5)	db 0,0,0,0,0,0,0,0									; 5-8191 records	
 
 GDT_size = GDT_DESCR - GDT
-GDTR			dw GDT_size-1
-				dd GDT
+GDTR	   dw GDT_size-1
+	   dd GDT
 
 outOfProg:
 	
