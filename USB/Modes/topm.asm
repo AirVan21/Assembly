929d1208c0da0f55cf9d4bb23fca652c7c2d1f82
AirVan21 <airvan21@gmail.com>
2014-11-30 12:31:34 +0300

    Added Code for PM

--- USB/Modes/topm.asm
+++ USB/Modes/topm.asm
@@ -7,30 +7,42 @@ _:
 
 ; CONSTS
 CMOS_PORT_ID   = 70h    ; Port for CMOS memory access	
+CODE_DESC_OFFS = 08h    ; Code Descriptor Offset
+DATA_DESC_OFFS = 10h    ; Data Descriptor Offset
 
 	jmp start  ;  Marker to place, where program starts	
 	
 start:
 
 set_prot_mode:
-	
-	call enable_interrupts   ; Disable Maskable && Non-Maskable interrupts
-	call setGDTpar           ; Calculate GDT size
-	lgdt GDTR                ;  
-	jmp outOfProg            ;
+
+	call enable_interrupts   		; Disable Maskable && Non-Maskable interrupts
+	call setGDTpar           		; Calculate GDT size
+	mov edi, dword ptr [GDTR]		; Load argument
+	lgdt [edi]               		;  
+	call CODE_DESC_OFFS:switchToPm  ; Setting in CS register Code Selector  
+	call enable_interrupts          ; Enable Maskable && Non-Maskable interrupts
+	jmp outOfProg            		;
+
+switchToPm:
+	mov ax, DATA_DESC_OFFS          ; Setting Data Selector
+	mov ds, ax                      ; 
+	mov es, ax                      ; Selector of Data Segment
+	mov ss, ax                      ; 
+	ret                             ;
 	
 setGDTpar:
-	xor eax, eax             ;
-	push bx                  ; Save bx
-	mov ax, ds               ; Segment addr
-	shl eax, 4               ; Segment*16
-	lea bx, GDT              ; Offset
-	add ax, bx               ; Linear Address calc
-	mov (GDTR+2), eax        ; Setting Address  
-	pop bx                   ; Recover bx
-	ret                      ;
+	xor eax, eax             		;
+	push bx                  		; Save bx
+	mov ax, ds               		; Segment addr
+	shl eax, 4               		; Segment*16
+	lea bx, GDT              		; Offset
+	add ax, bx               		; Linear Address calc
+	mov dword ptr [(GDTR+2)], eax   ; Setting Address  
+	pop bx                   		; Recover bx
+	ret                      		;
 	
-	
+; Disables Maskable && Non-Maskable interrupts
 disable_interrupts:
 	xor eax, eax         ;
 	push dx              ; Save dx
@@ -44,6 +56,7 @@ disable_interrupts:
 	pop dx               ; Recover dx
 	ret                  ;
 
+; Enables Maskable && Non-Maskable interrupts
 enable_interrupts:
 	xor eax, eax         ;
 	push dx              ; Save dx
@@ -57,19 +70,17 @@ enable_interrupts:
 	sti                  ; Enable Maskable interrupt
 	ret                  ;
 
-
-
 ; The main Global Descriptors Table (GDT), 8192 records 
 GDT_COUNT = 8192
 
 GDT:
-NULL_descr	db 0,0,0,0,0,0,0,0											; 0 zero descriptor
-CODE_descr	db 0FFh,0FFh,00h,00H,00H,10011010b, 11001111b,00h	; 1 code main code descriptor
+NULL_descr	db 0,0,0,0,0,0,0,0									; null descriptor (never referenced)
+CODE_descr	db 0FFh,0FFh,00h,00H,00H,10011010b, 11001111b,00h	; 1 code main code descriptor (kernel)
 DATA_descr	db 0FFh, 0FFh, 00h, 00h, 00h, 10010010b, 11001111b, 00h	; 2 data main descriptor
-GDT_16bitCS	db 0FFh, 0FFh, 0, 0, 0, 10011010b, 0, 0			; 3 16-bit code descriptor
-GDT_16bitDS	db 0FFh, 0FFh, 0, 0, 0, 10010010b, 0, 0			; 4 16-bit data descriptor
+GDT_16bitCS	db 0FFh, 0FFh, 0, 0, 0, 10011010b, 0, 0			    ; 3 16-bit code descriptor
+GDT_16bitDS	db 0FFh, 0FFh, 0, 0, 0, 10010010b, 0, 0		        ; 4 16-bit data descriptor
 
-gdt_free_cells         db (GDT_COUNT-1-5) DUP (6 DUP(0))                ; Setting zeros for (5-8191 records)	
+gdt_free_cells         db (GDT_COUNT-1-5) DUP (6 DUP(0))        ; Setting zeros for (5-8191 records)	
 
 GDT_DESCR:
 
